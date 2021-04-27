d629820bfa84c07eb5af4d8b481822898625ef83
Yacine Rezgui <rezgui.y@gmail.com>
2021-04-28 00:29:21 +0100

    Add ModernStorage Media package

---
+++ media/src/main/java/com/google/modernstorage/media/MediaCollection.kt
@@ -0,0 +1,57 @@
+package com.google.modernstorage.media
+
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+
+
+fun getImageCollection(location: StorageLocation): Uri? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        getImageCollectionApi29(location)
+    } else {
+        getImageCollectionApi21(location)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+private fun getImageCollectionApi29(location: StorageLocation): Uri? {
+    return when(location) {
+        Internal -> MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_INTERNAL)
+        SharedPrimary -> MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+    }
+}
+
+
+private fun getImageCollectionApi21(location: StorageLocation): Uri? {
+    return when(location) {
+        Internal -> MediaStore.Images.Media.INTERNAL_CONTENT_URI
+        SharedPrimary -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+    }
+}
+
+
+
+fun getVideoCollection(location: StorageLocation): Uri? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        getVideoCollectionApi29(location)
+    } else {
+        getVideoCollectionApi21(location)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+private fun getVideoCollectionApi29(location: StorageLocation): Uri? {
+    return when(location) {
+        Internal -> MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_INTERNAL)
+        SharedPrimary -> MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+    }
+}
+
+
+private fun getVideoCollectionApi21(location: StorageLocation): Uri? {
+    return when(location) {
+        Internal -> MediaStore.Video.Media.INTERNAL_CONTENT_URI
+        SharedPrimary -> MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+    }
+}
--- media/src/main/java/com/google/modernstorage/media/StorageLocation.kt
+++ media/src/main/java/com/google/modernstorage/media/StorageLocation.kt
@@ -1,47 +1,7 @@
 package com.google.modernstorage.media
 
-import android.graphics.Bitmap
-import android.net.Uri
-import android.os.storage.StorageVolume
-import java.io.OutputStream
-
 sealed class StorageLocation
 object Internal : StorageLocation()
 object SharedPrimary : StorageLocation()
+// TODO: Handle secondary shared storage
 //class SharedSecondary(val volume: StorageVolume) : StorageLocation()
-
-interface ModernStorageInterface {
-    fun createUri(filename: String, collection: Uri)
-    suspend fun addImage(
-        filename: String,
-        bitmap: Bitmap,
-        compressFormat: Bitmap.CompressFormat,
-        quality: Int = 100,
-        location: StorageLocation
-    )
-
-    suspend fun addImage(
-        filename: String,
-        bytes: ByteArray,
-        compressFormat: Bitmap.CompressFormat,
-        quality: Int = 100,
-        location: StorageLocation
-    )
-
-    suspend fun addImage(
-        filename: String,
-        stream: OutputStream,
-        compressFormat: Bitmap.CompressFormat,
-        quality: Int = 100,
-        location: StorageLocation
-    )
-
-    suspend fun modifyImage(
-        uri: Uri,
-        data: ByteArray,
-        compressFormat: Bitmap.CompressFormat,
-        quality: Int = 100,
-    )
-}
-
-class ModernStorage
