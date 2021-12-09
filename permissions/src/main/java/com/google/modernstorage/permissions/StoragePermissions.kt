b45a162f6bec1b222577c5d9773376fa90adfc1e
Yacine Rezgui <rezgui.y@gmail.com>
2021-12-09 00:39:28 +0000

    Fix permissions check logic

--- permissions/src/main/java/com/google/modernstorage/permissions/StoragePermissions.kt
+++ permissions/src/main/java/com/google/modernstorage/permissions/StoragePermissions.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.modernstorage.permissions
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
@@ -6,7 +21,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Environment
-import androidx.activity.result.ActivityResult
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.core.os.BuildCompat
@@ -18,15 +32,19 @@ class StoragePermissions(private val context: Context) {
         private const val AUDIO_PERMISSION = "READ_MEDIA_AUDIO"
 
         // File Types
-        const val IMAGES = 1
-        const val VIDEO = 2
-        const val AUDIO = 3
-        const val DOCUMENTS = 4
+        enum class FileType {
+            Image, Video, Audio, Document
+        }
 
         // Ownership type
-        const val SELF = 1
-        const val OTHER_APPS = 2
-        const val ALL = 3
+        enum class Ownership {
+            Self, AllApps
+        }
+
+        // Ownership type
+        enum class Action {
+            READ, READ_AND_WRITE
+        }
     }
 
     private fun checkPermission(permission: String): Boolean {
@@ -39,126 +57,93 @@ class StoragePermissions(private val context: Context) {
     }
 
     /**
-     * Check if app can read files
+     * Check if app can read shared files
      */
-    fun canReadFiles(types: List<Int>, ownership: Int): Boolean {
-        TODO("Not implemented yet")
-    }
-
-    /**
-     * Check if app can read its own document files
-     */
-    fun canReadOwnDocumentFiles(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            true
-        } else {
-            checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE)
+    fun canAccessFiles(action: Action, types: List<FileType>, ownership: Ownership): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && checkFullStoragePermission()) {
+            return true
         }
-    }
 
-    /**
-     * Check if app can read shared visual media files (images & video)
-     */
-    fun canReadSharedVisualMedia(): Boolean {
-        return when {
-            BuildCompat.isAtLeastT() -> {
-                checkFullStoragePermission() || checkPermission(VISUAL_PERMISSION)
+        val conditions = mutableListOf<Boolean>()
+        when (ownership) {
+            Ownership.Self -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    conditions.add(true)
+                } else {
+                    when (action) {
+                        Action.READ -> conditions.add(checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE))
+                        Action.READ_AND_WRITE -> conditions.add(checkPermission(WRITE_EXTERNAL_STORAGE))
+                    }
+                }
             }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
-                checkFullStoragePermission() || checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE)
-            }
-            else -> {
-                checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE)
-            }
-        }
-    }
-    fun requestReadSharedVisualMediaPermission(): ActivityResult = TODO("Have to do it")
+            Ownership.AllApps -> {
+                if (types.contains(FileType.Image)) {
+                    when {
+                        BuildCompat.isAtLeastT() -> {
+                            conditions.add(checkPermission(VISUAL_PERMISSION))
+                        }
+                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                            conditions.add(checkPermission(READ_EXTERNAL_STORAGE))
+                        }
+                        else -> {
+                            when (action) {
+                                Action.READ -> conditions.add(checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE))
+                                Action.READ_AND_WRITE -> conditions.add(checkPermission(WRITE_EXTERNAL_STORAGE))
+                            }
+                        }
+                    }
+                }
 
+                if (types.contains(FileType.Video)) {
+                    when {
+                        BuildCompat.isAtLeastT() -> {
+                            conditions.add(checkPermission(VISUAL_PERMISSION))
+                        }
+                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                            conditions.add(checkPermission(READ_EXTERNAL_STORAGE))
+                        }
+                        else -> {
+                            when (action) {
+                                Action.READ -> conditions.add(checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE))
+                                Action.READ_AND_WRITE -> conditions.add(checkPermission(WRITE_EXTERNAL_STORAGE))
+                            }
+                        }
+                    }
+                }
 
+                if (types.contains(FileType.Audio)) {
+                    when {
+                        BuildCompat.isAtLeastT() -> {
+                            conditions.add(checkPermission(AUDIO_PERMISSION))
+                        }
+                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                            conditions.add(checkPermission(READ_EXTERNAL_STORAGE))
+                        }
+                        else -> {
+                            when (action) {
+                                Action.READ -> conditions.add(checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE))
+                                Action.READ_AND_WRITE -> conditions.add(checkPermission(WRITE_EXTERNAL_STORAGE))
+                            }
+                        }
+                    }
+                }
 
-
-
-
-
-
-
-
-
-    /**
-     * Check if app can read shared audio media files
-     */
-    fun canReadSharedAudioMedia(): Boolean {
-        return when {
-            BuildCompat.isAtLeastT() -> {
-                checkFullStoragePermission() || checkPermission(AUDIO_PERMISSION)
-            }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
-                checkFullStoragePermission() || checkPermission(READ_EXTERNAL_STORAGE)
-            }
-            else -> {
-                checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE)
-            }
-        }
-    }
-    fun requestReadSharedAudioMediaPermission(): ActivityResult = TODO("Have to do it")
-
-    /**
-     * Check if app can write its own visual media files (images & video)
-     */
-    fun canWriteOwnVisualMedia(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            true
-        } else {
-            checkPermission(WRITE_EXTERNAL_STORAGE)
-        }
-    }
-    fun requestWriteOwnVisualMediaPermission(): ActivityResult = TODO("Have to do it")
-
-    /**
-     * Check if app can write its own audio media files
-     */
-    fun canWriteOwnAudioMedia(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            true
-        } else {
-            checkPermission(WRITE_EXTERNAL_STORAGE)
-        }
-    }
-    fun requestWriteOwnAudioMediaPermission(): ActivityResult = TODO("Have to do it")
-
-    /**
-     * Check if app can write shared visual media files (images & video)
-     */
-    fun canWriteSharedVisualMedia(): Boolean {
-        return when {
-            BuildCompat.isAtLeastT() -> {
-                checkFullStoragePermission() || checkPermission(AUDIO_PERMISSION)
-            }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
-                checkFullStoragePermission() || checkPermission(READ_EXTERNAL_STORAGE)
-            }
-            else -> {
-                checkPermission(WRITE_EXTERNAL_STORAGE)
+                if (types.contains(FileType.Document)) {
+                    when {
+                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                            conditions.add(false)
+                        }
+                        else -> {
+                            when (action) {
+                                Action.READ -> conditions.add(checkPermission(READ_EXTERNAL_STORAGE) || checkPermission(WRITE_EXTERNAL_STORAGE))
+                                Action.READ_AND_WRITE -> conditions.add(checkPermission(WRITE_EXTERNAL_STORAGE))
+                            }
+                        }
+                    }
+                }
             }
         }
-    }
-    fun requestWriteSharedVisualMediaPermission(): ActivityResult = TODO("Have to do it")
 
-    /**
-     * Check if app can write shared audio media files
-     */
-    fun canWriteSharedAudioMedia(): Boolean {
-        return when {
-            BuildCompat.isAtLeastT() -> {
-                checkFullStoragePermission() || checkPermission(AUDIO_PERMISSION)
-            }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
-                checkFullStoragePermission() || checkPermission(READ_EXTERNAL_STORAGE)
-            }
-            else -> {
-                checkPermission(WRITE_EXTERNAL_STORAGE)
-            }
-        }
+        return conditions.all { it }
     }
-    fun requestWriteSharedAudioMediaPermission(): ActivityResult = TODO("Have to do it")
 }
