96ee5be7dea1bbb1babe0ad218bcd229cea572cf
Andreas <andreas.everos@gmail.com>
2022-01-17 21:00:39 +0100

    Convert Groovy to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+plugins {
+    id("com.diffplug.spotless") version "5.12.5"
+    id("org.jetbrains.dokka") version "1.4.32"
+    id("me.tylerbwong.gradle.metalava") version "0.1.9" apply false
+    id("com.vanniktech.maven.publish") version "0.18.0" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.4.32" apply false
+}
+
+allprojects {
+    // TODO: Remove ext section when filesystem and mediastore is removed
+    ext {
+        set("kotlin_version", "1.5.31")
+        set("compose_version", "1.0.5")
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.withType<DokkaMultiModuleTask>().configureEach {
+    outputDirectory.set(rootProject.file("docs/api"))
+    failOnWarning.set(true)
+}
+
+subprojects {
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            ktlint("0.41.0")
+            licenseHeaderFile(rootProject.file("spotless/copyright.txt"))
+        }
+
+        groovyGradle {
+            target("**/*.gradle")
+            greclipse().configFile(rootProject.file("spotless/greclipse.properties"))
+            licenseHeaderFile(
+                rootProject.file("spotless/copyright.txt"),
+                "(buildscript|apply|import|plugins)"
+            )
+        }
+    }
+
+    if (project.hasProperty("POM_ARTIFACT_ID") && project.properties["POM_ARTIFACT_ID"] != "modernstorage-bom") {
+        apply<me.tylerbwong.gradle.metalava.plugin.MetalavaPlugin>()
+
+        metalava {
+            filename = "api/current.api"
+            reportLintsAsErrors = true
+        }
+    }
+}
+
+// Extension function due to metalava not having proper Kotlin DSL
+fun Project.metalava(configure: Action<me.tylerbwong.gradle.metalava.extension.MetalavaExtension>): Unit =
+    (this as ExtensionAware).extensions.configure("metalava", configure)
---
+++ storage/build.gradle.kts
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.stdlib.jdk7)
+    implementation(libs.coroutines.android)
+    androidTestImplementation(libs.coroutines.android)
+
+    implementation(androidx.corektx)
+    implementation(androidx.appcompat)
+    implementation(libs.okio)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(androidx.junit)
+    androidTestImplementation(androidx.junitktx)
+    androidTestImplementation(androidx.espresso)
+    androidTestImplementation(androidx.rules)
+    androidTestImplementation(androidx.uiautomator)
+    androidTestImplementation(androidx.runner)
+}
+repositories {
+    mavenCentral()
+}
