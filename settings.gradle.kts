fbb69aba60b69bf9e8686b0a7c76af77236e212f
Yacine Rezgui <rezgui.y@gmail.com>
2022-03-18 11:44:41 +0000

    Add refreshVersions Gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,11 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.40.1"
+}
+
 rootProject.name = "ModernStorage"
 
 include(":bom")
