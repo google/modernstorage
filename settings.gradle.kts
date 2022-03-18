ffc209b824602ba9609c727fe18d41df71b3d1f7
Yacine Rezgui <rezgui.y@gmail.com>
2022-03-18 14:52:11 +0000

    Remove version catalog feature preview

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,19 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-enableFeaturePreview("VERSION_CATALOGS")
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("compose") {
-            from(files("gradle/compose.versions.toml"))
-        }
-        create("androidx") {
-            from(files("gradle/androidx.versions.toml"))
-        }
-    }
-}
-
 plugins {
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.40.1"
