5e50362a9b38fc7616bdbc1932ed243460c78ada
Yacine Rezgui <yrezgui@google.com>
2022-04-13 07:43:45 +0100

    Add storage permissions checking as Context extension functions

--- permissions/src/main/java/com/google/modernstorage/permissions/RequestAccess.kt
+++ permissions/src/main/java/com/google/modernstorage/permissions/RequestAccess.kt
@@ -21,7 +21,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions.Companion.EXTRA_PERMISSION_GRANT_RESULTS
+//import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions.Companion.EXTRA_PERMISSION_GRANT_RESULTS
 import androidx.core.content.ContextCompat
 
 class RequestAccess : ActivityResultContract<RequestAccess.Args, Boolean>() {
@@ -69,9 +69,10 @@ class RequestAccess : ActivityResultContract<RequestAccess.Args, Boolean>() {
         if (intent == null) return false
         val permissions =
             intent.getStringArrayExtra(ActivityResultContracts.RequestMultiplePermissions.EXTRA_PERMISSIONS)
-        val grantResults = intent.getIntArrayExtra(EXTRA_PERMISSION_GRANT_RESULTS)
-        if (grantResults == null || permissions == null) return false
-
-        return grantResults.all { result -> result == PackageManager.PERMISSION_GRANTED }
+        return false
+//        val grantResults = intent.getIntArrayExtra(EXTRA_PERMISSION_GRANT_RESULTS)
+//        if (grantResults == null || permissions == null) return false
+//
+//        return grantResults.all { result -> result == PackageManager.PERMISSION_GRANTED }
     }
 }
--- permissions/src/main/java/com/google/modernstorage/permissions/StoragePermissions.kt
+++ permissions/src/main/java/com/google/modernstorage/permissions/StoragePermissions.kt
@@ -17,12 +17,158 @@ package com.google.modernstorage.permissions
 
 import android.Manifest.permission.MANAGE_EXTERNAL_STORAGE
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGE
+import android.Manifest.permission.READ_MEDIA_VIDEO
+import android.Manifest.permission.READ_MEDIA_AUDIO
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Environment
 import androidx.core.content.ContextCompat
+
+/**
+ * Type of files
+ */
+enum class FileType {
+    Image, Video, Audio, Document
+}
+
+/**
+ * Type of file ownership
+ */
+enum class CreatedBy {
+    Self, AllApps
+}
+
+/**
+ * Type of file actions
+ */
+enum class Action {
+    READ, READ_AND_WRITE
+}
+
+/**
+ * Check if app can read shared files
+ */
+fun Context.hasStorageReadAccessFor(
+    types: List<FileType>,
+    createdBy: CreatedBy
+): Boolean {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && Environment.isExternalStorageManager()) {
+        return true
+    }
+
+    val targetSdk = applicationInfo.targetSdkVersion
+
+    return getPermissions(Action.READ_AND_WRITE, types, createdBy, targetSdk).all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+}
+
+/**
+ * Check if app can read & write shared files
+ */
+fun Context.hasStorageReadWriteAccessFor(
+    types: List<FileType>,
+    createdBy: CreatedBy
+): Boolean {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && Environment.isExternalStorageManager()) {
+        return true
+    }
+
+    val targetSdk = applicationInfo.targetSdkVersion
+
+    return getPermissions(Action.READ_AND_WRITE, types, createdBy, targetSdk).all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+}
+
+internal fun getPermissions(
+    action: Action,
+    types: List<FileType>,
+    createdBy: CreatedBy,
+    targetSdk: Int
+): Set<String> {
+    val permissions = mutableSetOf<String>()
+
+    when (createdBy) {
+        CreatedBy.Self -> {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                permissions += when (action) {
+                    Action.READ -> READ_EXTERNAL_STORAGE
+                    Action.READ_AND_WRITE -> WRITE_EXTERNAL_STORAGE
+                }
+            }
+        }
+        CreatedBy.AllApps -> {
+            if (types.contains(FileType.Image)) {
+                permissions += when {
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && targetSdk >= Build.VERSION_CODES.TIRAMISU -> {
+                        READ_MEDIA_IMAGE
+                    }
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                        READ_EXTERNAL_STORAGE
+                    }
+                    else -> {
+                        when (action) {
+                            Action.READ -> READ_EXTERNAL_STORAGE
+                            Action.READ_AND_WRITE -> WRITE_EXTERNAL_STORAGE
+                        }
+                    }
+                }
+            }
+
+            if (types.contains(FileType.Video)) {
+                permissions += when {
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && targetSdk >= Build.VERSION_CODES.TIRAMISU -> {
+                        READ_MEDIA_VIDEO
+                    }
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                        READ_EXTERNAL_STORAGE
+                    }
+                    else -> {
+                        when (action) {
+                            Action.READ -> READ_EXTERNAL_STORAGE
+                            Action.READ_AND_WRITE -> WRITE_EXTERNAL_STORAGE
+                        }
+                    }
+                }
+            }
+
+            if (types.contains(FileType.Audio)) {
+                permissions += when {
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && targetSdk >= Build.VERSION_CODES.TIRAMISU -> {
+                        READ_MEDIA_AUDIO
+                    }
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                        READ_EXTERNAL_STORAGE
+                    }
+                    else -> {
+                        when (action) {
+                            Action.READ -> READ_EXTERNAL_STORAGE
+                            Action.READ_AND_WRITE -> WRITE_EXTERNAL_STORAGE
+                        }
+                    }
+                }
+            }
+
+            if (types.contains(FileType.Document)) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    return setOf(MANAGE_EXTERNAL_STORAGE)
+                } else {
+                    permissions += when (action) {
+                        Action.READ -> READ_EXTERNAL_STORAGE
+                        Action.READ_AND_WRITE -> WRITE_EXTERNAL_STORAGE
+                    }
+                }
+            }
+        }
+    }
+
+    return permissions
+}
+
 
 class StoragePermissions(private val context: Context) {
     companion object {
